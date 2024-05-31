be330c82efe656bff08fd15083d6cea01d30ddd3
luciarol <luciarol87@gmail.com>
2024-05-31 15:57:27 +0200

    conexión a base de datos y tabla usuario

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', function () {
+    return "Usuarios";
+});
+
+
+
+Route::get('/clientes', [ClienteController::class, 'Home']);
+
