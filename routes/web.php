3491cba6f361176ca9d5b86e2710ffeb65aa6680
luciarol <luciarol87@gmail.com>
2024-06-01 11:11:14 +0200

    breeze, blade

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ClienteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/usuarios', function () {
-    return "Usuarios";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-
-Route::get('/clientes', [ClienteController::class, 'Home']);
-
+require __DIR__.'/auth.php';
