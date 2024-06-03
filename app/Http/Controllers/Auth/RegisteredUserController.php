d3418923474f8a64e540acde2fa2cf3f34848027
luciarol <luciarol87@gmail.com>
2024-06-03 20:58:37 +0200

    email y bienvenida dashboard

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 
 class RegisteredUserController extends Controller
 {
@@ -44,6 +47,35 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+
+        $mail = new PHPMailer(true);
+
+        try {
+            // Configuración del servidor SMTP
+            $mail->isSMTP();
+            $mail->Host = env('MAIL_HOST'); // Lee el valor de MAIL_HOST del archivo .env
+            $mail->SMTPAuth = true;
+            $mail->Username = env('MAIL_USERNAME'); // Lee el valor de MAIL_USERNAME del archivo .env
+            $mail->Password = env('MAIL_PASSWORD'); // Lee el valor de MAIL_PASSWORD del archivo .env
+            $mail->Port = env('MAIL_PORT'); // Lee el valor de MAIL_PORT del archivo .env
+            $mail->SMTPSecure = env('MAIL_ENCRYPTION'); // Lee el valor de MAIL_ENCRYPTION del archivo .env
+
+            // Detalles del remitente y del destinatario
+            $mail->setFrom(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME')); // Lee los valores de MAIL_FROM_ADDRESS y MAIL_FROM_NAME del archivo .env
+            $mail->addAddress($request->email, $request->name);
+
+            // Contenido del correo
+            $mail->isHTML(true);
+            $mail->Subject = 'Registro en aplicación de tatuajes';
+            $mail->Body = 'Acabas de registrarte';
+
+            // Envío del correo
+            $mail->send();
+        
+        } catch (Exception $e) {
+            
+        }
+
 
         return redirect(route('dashboard', absolute: false));
     }
